b935157c0ffb99a1b3dfdf8cdeb513a8baa3da56
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2023-01-05 16:27:05 +0300

    Fetch API, Servisler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("tayfun")
+  const [avatar, setAvatar] = useState(false)
+
+  const [users, setUsers] = useState(false)
+
+  const addPost = data => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }
+
+
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+
+    .then(res =>{
+      if(res.ok && res.status == 200){
+        return res.json()
+      }
+      
+    })
+    .then(data => setUsers(data))
+    .catch(err => console.log(err))
+
+    addPost({
+      userId: 1,
+      title: 'Örnek Post',
+      body: 'Post İçeriği'
+    })
+  },[])
+
+  const submitHandle = e =>{
+    e.preventDefault()
+    console.log("submit edildi")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandle}>
+        <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/> <br />
+        <input type="file" name="avatar" onChange={e => setAvatar(e.target.files[0])} /> <br />
+        <button type="submit" disabled={!name || !avatar}>Kaydet</button>
+      </form>
+
+
+      <h1>Names</h1>
+      <ul>
+      {users && users.map(user=>(
+        <li key={user.id}>
+          {user.name}
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
